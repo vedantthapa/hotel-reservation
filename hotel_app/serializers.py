13a212a57d8ab62b65f5f14f023a9714a3c23904
vedantthapa <thapavedant@gmail.com>
2022-03-10 12:34:59 -0400

    Fixes POST request

--- hotel_app/serializers.py
+++ hotel_app/serializers.py
@@ -20,3 +20,12 @@ class BookingSerializer(ModelSerializer):
     class Meta:
         model = Booking
         fields = ["hotel", "check_in", "check_out", "guest"]
+
+    def create(self, validated_data):
+        guest_data = validated_data.pop('guest')
+        booking = Booking.objects.create(**validated_data)
+        guest_set_serializer = self.fields['guest']
+        for each in guest_data:
+            each['booking'] = booking
+        guest_set_serializer.create(guest_data)
+        return booking
