221276ffb7f3142e2f826bcd96aa7852bee795b7
vedantthapa <thapavedant@gmail.com>
2022-03-10 16:26:54 -0400

    Returns confirmation number after POST request

--- hotel_app/views.py
+++ hotel_app/views.py
@@ -1,6 +1,7 @@
 from .models import *
 from .serializers import *
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 
 class Hotel_list(generics.ListCreateAPIView):
@@ -11,3 +12,11 @@ class Hotel_list(generics.ListCreateAPIView):
 class Booking_List(generics.ListCreateAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+    def post(self, request, format=None):
+        serializer = BookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            booking_id = serializer.data.pop('booking_id')
+            return Response(f"Your confirmation number is: {booking_id}", status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
