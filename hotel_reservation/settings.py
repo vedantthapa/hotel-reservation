74c08b5b14ee11a403a3a64e0a386958b8dd6d3a
vedantthapa <thapavedant@gmail.com>
2022-03-10 11:35:04 -0400

    Changes database to MySQL

--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'hotel_reservation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vv_thapa',
+        'USER': 'vv_thapa',
+        'PASSWORD': 'A00457249!cda',
+        'HOST': 'dev.cs.smu.ca',
     }
 }
 
