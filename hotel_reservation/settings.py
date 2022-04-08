ecb873e2d0e19c9dc4bc81f93bb18fc64cfe5fdc
vedantthapa <thapavedant@gmail.com>
2022-04-08 03:52:35 -0300

    Edits ALLOWED_HOSTS list

--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z312554hpzmrtay*3c(6kedq@^et_momi#$^u*#-&j=#ob=3di
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["10.0.2.2", "127.0.0.1"]
 
 
 # Application definition
